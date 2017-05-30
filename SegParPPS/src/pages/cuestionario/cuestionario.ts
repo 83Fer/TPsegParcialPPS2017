31189288e5a81b90db30e3a523e87f8e49e37f69
BelenMayo <belenmaydi@hotmail.com>
2017-05-29 22:35:49 -0300

    Página cuestionario

--- SegParPPS/src/pages/cuestionario/cuestionario.ts
+++ SegParPPS/src/pages/cuestionario/cuestionario.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { AuthProvider } from './../../providers/auth/auth';
 
 @Component({
@@ -9,11 +9,56 @@ import { AuthProvider } from './../../providers/auth/auth';
 export class CuestionarioPage {
 
   public user: string;
-  constructor(private auth: AuthProvider) {
+
+  titulo: string= "Cursos"
+	mensaje: string= ""
+  curso: string= ""
+
+  constructor(private auth: AuthProvider,public alertCtrl: AlertController,) {
    
     this.auth.getUserData().subscribe(data => {
       this.user=data.name;
       //console.log(this.user);  
     });
+  }
+
+  seleccionarCurso(){
+    let ventana = this.alertCtrl.create({
+                  title: this.titulo,
+                  message: this.mensaje, 
+                  buttons:[
+                    {
+                      text: "1ºA",
+                      handler: data => {
+                        console.log('Curso: 1ºA');
+                        this.curso= "1ºA";
+                        }
+                      },
+                      
+                    {
+                      text: "1ºB",
+                      handler: data => {
+                        console.log('Curso: 1ºB');
+                        this.curso= "1ºB";
+                        }
+                      },
+                    {
+                      text: "2ºA",
+                      handler: data => {
+                        console.log('Curso: 2ºA');
+                        this.curso= "2ºA";
+                        }
+                      },
+                    {
+                      text: "2ºB",
+                      handler: data => {
+                        console.log('Curso: 2ºB');
+                        this.curso= "2ºB";
+                        }
+                      }
+                    ]
+
+                  });
+    ventana.present(ventana);
   }
 }
